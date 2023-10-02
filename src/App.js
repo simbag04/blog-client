5379816c37e5b9bf070fc4cfea6d6aaac9ab8960
simbag04 <simbag04@gmail.com>
2023-10-02 00:37:13 -0700

    Change API Link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export const ApiContext = createContext();
 function App() {
   return (
     <div className="App">
-      <ApiContext.Provider value={ "https://pronghorn-leg-warmers.cyclic.app" }>
+      <ApiContext.Provider value={ "https://blog-api-1r4y.onrender.com" }>
         <Main></Main>
       </ApiContext.Provider>
     </div>
